889c80706c2242449cd2b913b2d888500676258a
Ipadeola Taiwo <ipadeola.it@gmail.com>
2023-04-16 11:39:04 +0100

    complete the header component make it dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const [stateChange, setStateChange] = useState("Sign In");
   const { pathname } = useLocation();
   const navigate = useNavigate();
 
-  const pathMatch = (path) => path === pathname && true;
+  const pathMatch = (path) => path === pathname;
+  const auth = getAuth();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setStateChange("Profile");
+      } else {
+        setStateChange("Sign In");
+      }
+    });
+  }, [auth]);
 
   return (
     <div className="bg-white border-b sticky top-0 z-50">
@@ -23,7 +36,7 @@ const Header = () => {
           <ul className="flex space-x-10">
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-500 border-b-[3px] border-b-transparent ${
-                pathMatch("/") && `text-zinc-950 border-b-rose-950`
+                pathMatch("/") && ` text-zinc-950 border-b-red-700`
               }`}
               onClick={() => navigate("/")}
             >
@@ -31,19 +44,21 @@ const Header = () => {
             </li>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-500 border-b-[3px] border-b-transparent ${
-                pathMatch("/offers") && `text-zinc-950 border-b-rose-950`
+                pathMatch("/offers") && ` text-zinc-950 border-b-red-700`
               }`}
               onClick={() => navigate("/offers")}
             >
               Offers
             </li>
+
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-500 border-b-[3px] border-b-transparent ${
-                pathMatch("/sign-in") && `text-zinc-950 border-b-rose-950`
+                (pathMatch("/sign-in") || pathMatch("/profile")) &&
+                ` text-zinc-950 border-b-red-700`
               }`}
-              onClick={() => navigate("/sign-in")}
+              onClick={() => navigate("/profile")}
             >
-              Sign In
+              {stateChange}
             </li>
           </ul>
         </div>
